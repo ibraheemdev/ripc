c6cc77d41c5835e5b8966680dc3b18c34259c93e
ibraheemdev <ibrah1440@gmail.com>
2021-05-25 18:59:20 -0400

    dummy compiler

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+fn main() {
+    let arg = std::env::args()
+        .nth(1)
+        .map(|x| x.parse::<usize>().ok())
+        .flatten();
+
+    match arg {
+        Some(num) => {
+            println!("  .globl main");
+            println!("main:");
+            println!("mov ${}, %rax", num);
+            println!("  ret");
+        }
+        None => {
+            eprintln!("error: invalid number of arguments");
+            std::process::exit(1);
+        }
+    }
+}
