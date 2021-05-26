c95155eef6770365f29102a830d48f57c0395541
ibraheemdev <ibrah1440@gmail.com>
2021-05-26 06:44:49 -0400

    lexer

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,129 @@
-use std::iter::Peekable;
+use std::collections::VecDeque;
+use std::io::Write;
+
+macro_rules! error {
+    ($($arg:tt)*) => {{
+        eprint!("error: ");
+        eprint!($($arg)*);
+        eprint!("\n");
+        std::io::stdout().flush().unwrap();
+        std::process::exit(1);
+    }}
+}
+
+macro_rules! next_num {
+    ($tokens:ident) => {
+        match $tokens.next() {
+            Some(Token::Num(num)) => num,
+            _ => error!("expected number"),
+        }
+    };
+}
 
 fn main() {
-    let tokens = std::env::args().nth(1).unwrap_or_else(|| {
+    let input = std::env::args().nth(1).unwrap_or_else(|| {
         eprintln!("error: invalid number of arguments");
         std::process::exit(1);
     });
 
-    let mut tokens = tokens.chars().peekable();
+    let mut tokens = lex(input);
 
     println!("  .globl main");
     println!("main:");
 
-    println!("  mov ${}, %rax", take_digits(&mut tokens));
+    println!("  mov ${}, %rax", next_num!(tokens));
 
     while let Some(token) = tokens.next() {
         match token {
-            '+' => {
-                println!("  add ${}, %rax", take_digits(&mut tokens));
-                continue;
+            Token::Add => {
+                println!("  add ${}, %rax", next_num!(tokens));
             }
-            '-' => {
-                println!("  sub ${}, %rax", take_digits(&mut tokens));
-                continue;
+            Token::Sub => {
+                println!("  sub ${}, %rax", next_num!(tokens));
             }
-            x => {
-                eprintln!("error: unexpected character {}", x);
-                std::process::exit(1);
+            Token::Eof => {
+                break;
             }
+            _ => error!("invalid token"),
         }
     }
 
     println!("  ret");
 }
 
-fn take_digits(tokens: &mut Peekable<std::str::Chars>) -> u32 {
-    let mut res = 0;
-    while let Some(c) = tokens.peek() {
-        match c.to_digit(10) {
-            Some(i) => res = res * 10 + i,
+#[derive(Debug, PartialEq, Eq)]
+enum Token {
+    Add,
+    Sub,
+    Num(usize),
+    Eof,
+}
+
+#[derive(Debug)]
+struct Tokens(VecDeque<Token>);
+
+impl Tokens {
+    fn next(&mut self) -> Option<Token> {
+        self.0.pop_front()
+    }
+}
+
+fn lex(input: String) -> Tokens {
+    let mut tokens = VecDeque::new();
+    let bytes = input.into_bytes();
+
+    let mut i = 0;
+    while let Some(&byte) = bytes.get(i) {
+        match byte {
+            b'+' => {
+                tokens.push_back(Token::Add);
+                i += 1;
+                continue;
+            }
+            b'-' => {
+                tokens.push_back(Token::Sub);
+                i += 1;
+                continue;
+            }
+            b'0'..=b'9' => {
+                let (num, len) = take_num(&bytes[i..]);
+                tokens.push_back(Token::Num(num));
+                i += len;
+                continue;
+            }
+            b if b.is_ascii_whitespace() => {
+                i += 1;
+                continue;
+            }
+            _ => {
+                error!("invalid token");
+            }
+        }
+    }
+
+    tokens.push_back(Token::Eof);
+    return Tokens(tokens);
+}
+
+fn take_num(bytes: &[u8]) -> (usize, usize) {
+    let mut val = 0_usize;
+    let mut len = 0;
+
+    for &byte in bytes {
+        match byte_to_ascii_digit(byte) {
+            Some(digit) => {
+                val = val * 10 + digit as usize;
+                len += 1;
+            }
             None => break,
         }
-        tokens.next();
     }
-    res
+    (val, len)
+}
+
+fn byte_to_ascii_digit(byte: u8) -> Option<u8> {
+    match byte {
+        b'0'..=b'9' => Some(byte - b'0'),
+        _ => None,
+    }
 }
