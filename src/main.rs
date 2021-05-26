8159021de4f26bf583b80c17520bba1cd901ec13
ibraheemdev <ibrah1440@gmail.com>
2021-05-25 20:04:45 -0400

    addition, subtraction

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
-fn main() {
-    let arg = std::env::args()
-        .nth(1)
-        .map(|x| x.parse::<usize>().ok())
-        .flatten();
+use std::iter::Peekable;
 
-    match arg {
-        Some(num) => {
-            println!("  .globl main");
-            println!("main:");
-            println!("mov ${}, %rax", num);
-            println!("  ret");
-        }
-        None => {
-            eprintln!("error: invalid number of arguments");
-            std::process::exit(1);
+fn main() {
+    let tokens = std::env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("error: invalid number of arguments");
+        std::process::exit(1);
+    });
+
+    let mut tokens = tokens.chars().peekable();
+
+    println!("  .globl main");
+    println!("main:");
+
+    println!("  mov ${}, %rax", take_digits(&mut tokens));
+
+    while let Some(token) = tokens.next() {
+        match token {
+            '+' => {
+                println!("  add ${}, %rax", take_digits(&mut tokens));
+                continue;
+            }
+            '-' => {
+                println!("  sub ${}, %rax", take_digits(&mut tokens));
+                continue;
+            }
+            x => {
+                eprintln!("error: unexpected character {}", x);
+                std::process::exit(1);
+            }
         }
     }
+
+    println!("  ret");
+}
+
+fn take_digits(tokens: &mut Peekable<std::str::Chars>) -> u32 {
+    let mut res = 0;
+    while let Some(c) = tokens.peek() {
+        match c.to_digit(10) {
+            Some(i) => res = res * 10 + i,
+            None => break,
+        }
+        tokens.next();
+    }
+    res
 }
